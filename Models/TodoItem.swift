b9393c8859350dfbf0c49f563afd6350b9473c3c
DahDayy <tot.david.ott@gmail.com>
2026-07-07 20:05:43 -0400

    ...

--- Models/TodoItem.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct TodoItem: Identifiable, Codable, Equatable {
-    var id: UUID = UUID()
-    var title: String
-    var isDone: Bool = false
-    var createdAt: Date = Date()
-}
